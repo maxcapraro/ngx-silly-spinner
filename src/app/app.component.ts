364b518d651726b268604e7736789ff6b44bc734
Maximilian Capraro <mcapraro@gmx.de>
2018-01-04 00:38:12 +0100

    Wired in actual silly-spinner component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  fullfilledObservable: Observable<string>;
+
+  pendingObservable: Observable<string>;
+
+  rejectedObservable: Observable<string>;
+
+  ngOnInit(): void {
+
+    this.fullfilledObservable = Observable.create(function(observer) {
+      observer.next('Test Value');
+    });
+
+    this.rejectedObservable = Observable.create();
+  }
+
 }
--- src/app/modules/silly-spinner/silly-spinner.component.ts
+++ src/app/modules/silly-spinner/silly-spinner.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/first';
 
 @Component({
   selector: 'silly-spinner',
   templateUrl: './silly-spinner.component.html',
   styleUrls: ['./silly-spinner.component.css']
 })
-export class SillySpinnerComponent implements OnInit {
+export class SillySpinnerComponent implements OnChanges {
 
-  constructor() { }
+  @Input()
+  promise: Promise<any>;
 
-  ngOnInit() {
+  @Input()
+  observable: Observable<any>;
+
+  @Input()
+  inline: boolean = false;
+
+  isPending: boolean;
+
+  error: Error;
+
+  ngOnChanges(): void {
+    this.initialize();
+  }
+
+  private initialize(): void {
+    this.isPending = true;
+    this.error = null;
+
+    if (this.promise) {
+      this.promise
+        .then(res => this.isPending = false)
+        .catch(err => this.error = new Error(err));
+    }
+    if (this.observable) {
+      this.observable.first().subscribe(
+        () => this.isPending = false,
+        (error) => this.error = new Error(error)
+      );
+    }
+  }
+
+  getIsPending(): boolean {
+    return this.isPending && this.error === null;
+  }
+
+  getIsFullfilled(): boolean {
+    return !this.isPending && this.error === null;
+  }
+
+  getIsRejected(): boolean {
+    return this.error !== null;
   }
 
 }
